101dc0b2bf9b54f3f816663bd998374320dec98b
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2020-12-14 11:43:24 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 
 # 1. TASK: print "Hello World"
 print("Hello World")
-# 2. print "Hello Noelle!" with the name in a variable
+# 2. print "Hello Noelle!" with the name in a variable 
 name = "Noelle"
 print("Hello" , name)	# with a comma
 print("Hello " + name)	# with a +
